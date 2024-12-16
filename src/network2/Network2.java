02a0c1457489320629bd94ec06fd94867bb7d6d7
islam atallah <atallahislam006@gmail.com>
2024-12-16 17:41:28 +0200

    Initial commit

---
+++ src/network2/Network2.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package network2;
+
+/**
+ *
+ * @author islam atallah
+ */
+public class Network2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        new frame1().setVisible(true);
+        // TODO code application logic here
+    }
+    
+}
